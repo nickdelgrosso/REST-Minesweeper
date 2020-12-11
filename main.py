18ac05fcdb90be6b393cf58e9431a0982f152c0a
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-12-11 12:24:16 +0100

    team submission works

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from minesweeper.entitites import Team
 
 app = FastAPI()
 
@@ -8,16 +9,27 @@ app = FastAPI()
 class Message(BaseModel):
     message: str
 
+
 @app.get("/", response_model=Message)
 async def root():
     return {"message": "Welcome to Lean Minesweeper!"}
 
+
+class RegistrationRequest(BaseModel):
+    team_name: str
 
 class RegistrationResponse(BaseModel):
     registered: bool
     team_name: str
     team_id: str
 
-@app.get("/register")
-async  def register(team_name: str):
-    return {"message": "registered"}
+
+@app.post("/register", response_model=RegistrationResponse)
+async def register(request: RegistrationRequest):
+    team = Team.init(name=request.team_name)
+    response = RegistrationResponse(
+        registered=True,
+        team_name=team.name,
+        team_id=team.id
+    )
+    return response
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
 
+
 def test_ping_works():
     r = requests.get("http://localhost:8000")
     assert r.ok
@@ -8,12 +9,33 @@ def test_ping_works():
     assert 'message' in msg
 
 
-def teams_can_register():
+def test_teams_can_register():
     r = requests.post(
         "http://localhost:8000/register",
-        data={
+        json={
             "team_name": "TestTeam",
         }
     )
     assert r.ok
-    
+    data = r.json()
+    assert data['registered'] is True
+    assert data['team_name'] == "TestTeam"
+    assert len(data['team_id']) >= 8 and isinstance(data['team_id'], str)
+
+
+def test_teams_get_unique_ids():
+    r = requests.post(
+        "http://localhost:8000/register",
+        json={
+            "team_name": "TestTeam1",
+        }
+    )
+    r2 = requests.post(
+        "http://localhost:8000/register",
+        json={
+            "team_name": "TestTeam1",
+        }
+    )
+    t1, t2 = r.json(), r2.json()
+    assert t1['team_name'] == t2['team_name']
+    assert t1['team_id'] != t2['team_id']
