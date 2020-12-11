d50ffd7aa4bb254f986fddc1b51122c49fe11c1e
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-12-11 17:58:11 +0100

    removed ping module

--- api/app.py
+++ api/app.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
+from pydantic.main import BaseModel
 
-from api.routes import ping, register_team, new_game
+from api.routes import register_team, new_game
 from minesweeper.cases.list_teams import PublicTeamListResponse
 from minesweeper.cases.reset_session import ResetResponse, ResetRequest
 from minesweeper.use_cases import UseCaseProvider
 
 app = FastAPI()
-app.include_router(ping.router)
 app.include_router(register_team.router)
 app.include_router(new_game.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Lean Minesweeper!"}
 
 
 @app.post("/reset", response_model=ResetResponse)
