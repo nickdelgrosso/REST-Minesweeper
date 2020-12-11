ca80045de019faa234829e76a7d8c04304e57fd2
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-12-11 16:55:12 +0100

    moved api code to own directory

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routes import ping, register_team, list_teams, reset_session, new_game
+from api.routes import ping, register_team, list_teams, reset_session, new_game
 
 app = FastAPI()
 app.include_router(ping.router)
@@ -8,11 +8,3 @@ app.include_router(register_team.router)
 app.include_router(list_teams.router)
 app.include_router(reset_session.router)
 app.include_router(new_game.router)
-
-#
-# class NewGameRequest(BaseModel):
-#     team_id: str
-#
-# @app.post("/new-game")
-# async def new_game(request: NewGameRequest):
-#     ...
