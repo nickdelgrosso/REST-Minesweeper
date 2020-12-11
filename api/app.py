58ef8de15770fc3d87c40d52b75ca2ab2603ad72
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-12-11 17:45:10 +0100

    removed module

--- api/app.py
+++ api/app.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 
-from api.routes import ping, register_team, list_teams, reset_session, new_game
+from api.routes import ping, register_team, list_teams, new_game
+from minesweeper.cases.reset_session import ResetResponse, ResetRequest
+from minesweeper.use_cases import UseCaseProvider
 
 app = FastAPI()
 app.include_router(ping.router)
 app.include_router(register_team.router)
 app.include_router(list_teams.router)
-app.include_router(reset_session.router)
 app.include_router(new_game.router)
+
+
+@app.post("/reset", response_model=ResetResponse)
+async def reset_session(request: ResetRequest) -> ResetResponse:
+    use_cases = UseCaseProvider()
+    return use_cases.reset_session(request=request)
